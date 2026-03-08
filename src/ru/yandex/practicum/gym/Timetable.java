7295738f87755358cbfcc2571c0a81c9ce88a577
Emil-Guseinov <guseinov-emil2018@yandex.ru>
2026-03-08 21:19:40 +0300

    CodeStyle

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class Timetable {
 
-    private  Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
-
+    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
 
@@ -17,56 +16,51 @@ public class Timetable {
             dayTrainings = new TreeMap<>();
             timetable.put(dayOfWeek, dayTrainings);
         }
-
         List<TrainingSession> trainingSessions = dayTrainings.get(timeOfDay);
         if (Objects.isNull(trainingSessions)) {
-        trainingSessions = new ArrayList<>();
-        dayTrainings.put(timeOfDay, trainingSessions);
-    }
+            trainingSessions = new ArrayList<>();
+            dayTrainings.put(timeOfDay, trainingSessions);
+        }
         trainingSessions.add(trainingSession);
-
-
     }
-
-    public TreeMap<TimeOfDay,List <TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        TreeMap<TimeOfDay, List<TrainingSession>> training = timetable.get(dayOfWeek) ;
+    public TreeMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        TreeMap<TimeOfDay, List<TrainingSession>> training = timetable.get(dayOfWeek);
         return Objects.nonNull(training) ? training : new TreeMap<>();
     }
 
     public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        TreeMap<TimeOfDay, List<TrainingSession>> dayTraining = timetable.get(dayOfWeek) ;
+        TreeMap<TimeOfDay,List<TrainingSession>> dayTraining = timetable.get(dayOfWeek);
         if (Objects.isNull(dayTraining)) {
             return new ArrayList<>();
         }
-        List<TrainingSession>  timeTrainings = dayTraining.get(timeOfDay) ;
+        List<TrainingSession> timeTrainings = dayTraining.get(timeOfDay);
         return Objects.nonNull(timeTrainings) ? timeTrainings : new ArrayList<>();
     }
     public List<TrainingSession> getAllSessions() {
         List<TrainingSession> all = new ArrayList<>();
-        for (TreeMap < TimeOfDay, List < TrainingSession>>dayTrainings: timetable.values()){
+        for (TreeMap<TimeOfDay,List<TrainingSession>> dayTrainings : timetable.values()) {
             for (List<TrainingSession> timeSessions : dayTrainings.values()) {
                 all.addAll(timeSessions);
             }
         }
         return all;
     }
-    public List<Map.Entry<Coach,Integer>> getCountByCoaches () {
+
+    public List<Map.Entry<Coach,Integer>> getCountByCoaches() {
         List<TrainingSession> allSessions = getAllSessions();
 
-            if (allSessions.isEmpty()) {
-                return  new ArrayList<>();
-            }
+        if (allSessions.isEmpty()) {
+            return new ArrayList<>();
+        }
 
-        Map<Coach, Integer> count = new HashMap<>();
+        Map<Coach,Integer> count = new HashMap<>();
 
         for (TrainingSession session : allSessions) {
             Coach coach = session.getCoach();
-            count.put(coach, count.getOrDefault(coach, 0) + 1);
-
+            count.put(coach,count.getOrDefault(coach, 0) + 1);
         }
-
-            List<Map.Entry<Coach, Integer>> result = new ArrayList<>(count.entrySet());
-            result.sort((a,b) -> b.getValue().compareTo(a.getValue()));
+        List<Map.Entry<Coach,Integer>> result = new ArrayList<>(count.entrySet());
+        result.sort((a, b) -> b.getValue().compareTo(a.getValue()));
 
         return result;
     }
